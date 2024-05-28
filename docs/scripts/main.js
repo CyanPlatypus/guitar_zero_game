e44fe42fb4e98f79eed65738db456c51d1bb5f2e
CyanPlatypus <CyanPlatypus@mail.ru>
2024-05-28 22:05:37 +0200

    Move rectangle based on time

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,17 +1,27 @@
 window.addEventListener("load", onLoad);
 
+const canvasVerticalPassTimeMs = 2000;
+
 var canvas;
 var context;
 
-function onLoad() {
+var x = 50;
+var y = 0;
+var noteVelocity = 0;
+var songStartTime = 0;
 
+function onLoad() {
     canvas = document.getElementById("canvas");
     context = canvas.getContext("2d");
 
     canvas.addEventListener("click", onPlayClick);
+
+    noteVelocity = canvas.height / canvasVerticalPassTimeMs;
 }
 
 function onPlayClick() {
+    songStartTime = Date.now();
+
     intervalId = setInterval(gameLoop, 15);
 }
 
@@ -21,12 +31,13 @@ function gameLoop() {
 }
 
 function updateState() {
-
+    currentSongTimeMs = Date.now() - songStartTime;
+    y = noteVelocity * currentSongTimeMs;
 }
 
 function draw() {
     clearCanvas();
-    drawRectangle(10, 10, 40, 50);
+    drawRectangle(x, y, 40, 50);
 }
 
 function drawRectangle(x, y, h, w) {
